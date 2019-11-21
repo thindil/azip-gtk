fd129feae251a7eeb0d2acfe941b87ff1aba380e
Bartek thindil Jasicki <thindil@laeran.pl>
2019-11-21 09:37:28 +0100

    added documentation

--- src/mainwindow.ads
+++ src/mainwindow.ads
@@ -23,16 +23,64 @@ with Gtk.Tree_Model; use Gtk.Tree_Model;
 with Gtk.Window; use Gtk.Window;
 with Gtkada.MDI; use Gtkada.MDI;
 
+-- ****h* AZipGtk/MainWindow
+-- FUNCTION
+-- Provides code for the main window of the program
+-- SOURCE
 package MainWindow is
+-- ****
 
+   -- ****v* MainWindow/Window
+   -- FUNCTION
+   -- Main window of the program.
+   -- SOURCE
    Window: Gtk_Window;
+   -- ****
+
+   -- ****v* MainWindow/MWindow
+   -- FUNCTION
+   -- MDI window which contains all open archives data
+   -- SOURCE
    MWindow: MDI_Window;
+   -- ****
 
-
+   -- ****f* MainWindow/NewArchive
+   -- FUNCTION
+   -- Create new archive view.
+   -- PARAMETERS
+   -- Self - Gtk_Tool_Button which was pressed. Unused, can be null.
+   -- SOURCE
    procedure NewArchive(Self: access Gtk_Tool_Button_Record'Class);
+   -- ****
+
+   -- ****f* MainWindow/OpenFile
+   -- FUNCTION
+   -- Open selected file and show it content to the user.
+   -- PARAMETERS
+   -- FileName - Full path to the archive which should be open.
+   -- SOURCE
    procedure OpenFile(FileName: String);
+   -- ****
+
+   -- ****f* MainWindow/DeleteItems
+   -- FUNCTION
+   -- Remove selected file or directory from selected archive.
+   -- PARAMETERS
+   -- Model - Gtk_Tree_Model with names of all files and directories in
+   --         selected archive
+   -- Path  - Gtk_Tree_Path to selected file or directory in selected archive.
+   --         Unused.
+   -- Iter  - Gtk_Tree_Iter to selected fiel or directory in selected archive.
+   -- SOURCE
    procedure DeleteItems
      (Model: Gtk_Tree_Model; Path: Gtk_Tree_Path; Iter: Gtk_Tree_Iter);
+   -- ****
+
+   -- ****f* MainWindow/CreateMainWindow
+   -- FUNCTION
+   -- Create main window of the program.
+   -- SOURCE
    procedure CreateMainWindow;
+   -- ****
 
 end MainWindow;

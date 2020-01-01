10bf6d0c35b82549e4c8afeab3c5bdd8c2c87a36
Bartek thindil Jasicki <thindil@laeran.pl>
2020-01-01 07:14:43 +0100

    added new code

---
+++ src/aziptk.adb
@@ -0,0 +1,40 @@
+-- Temporary file just to test some things :)
+--------------------------------------------------------------------
+--
+--  twashell.adb -- Tcl Windowing Ada SHell.  This program is the Ada version
+--              of the wish program included in the Tk distribution.
+--
+--  Copyright (c) 1995-1997 Terry J. Westley
+--
+--  See the file "license.htm" for information on usage and
+--  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+--
+--
+--------------------------------------------------------------------
+
+with CArgv; use CArgv;
+with Interfaces.C; use Interfaces.C;
+with Tcl.Tk;
+with Tcl;
+with TWashApp;
+
+procedure aziptk is --  Tcl Windowing Ada SHell
+
+   package C renames Interfaces.C;
+
+   --  Argc and Argv include the command name
+   Argc : C.int;
+   Argv : CArgv.Chars_Ptr_Ptr;
+
+begin --  TWASHell
+
+   --  Get command-line arguments and put them into C-style "argv,"
+   --  as required by Tk_Main.
+   CArgv.Create (Argc, Argv);
+   Argc := Argc + 1;
+   Argv := Argv & "mainwindow.tcl";
+
+   --  Start Tcl
+   Tcl.Tk.Tk_Main (Argc, Argv, TWashApp.Init'Access);
+
+end aziptk;

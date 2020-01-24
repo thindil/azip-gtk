5439ab220226cc302a5903b49db89b3e00ecfee4
Bartek thindil Jasicki <thindil@laeran.pl>
2020-01-24 07:14:41 +0100

    continue work on archives view

--- src/archivesviews.adb
+++ src/archivesviews.adb
@@ -20,16 +20,19 @@
 
 with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Interfaces.C.Strings; use Interfaces.C.Strings;
 with Tcl; use Tcl;
 with Tcl.Ada;
 with Tcl.Tk.Ada; use Tcl.Tk.Ada;
+with Tcl.Tk.Ada.Grid;
 with Tcl.Tk.Ada.Pack;
 with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
 with Tcl.Tk.Ada.Widgets.TtkButton; use Tcl.Tk.Ada.Widgets.TtkButton;
 with Tcl.Tk.Ada.Widgets.TtkFrame; use Tcl.Tk.Ada.Widgets.TtkFrame;
 with Tcl.Tk.Ada.Widgets.TtkLabel; use Tcl.Tk.Ada.Widgets.TtkLabel;
 with Tcl.Tk.Ada.Widgets.TtkPanedWindow; use Tcl.Tk.Ada.Widgets.TtkPanedWindow;
+with Tcl.Tk.Ada.Widgets.TtkScrollbar; use Tcl.Tk.Ada.Widgets.TtkScrollbar;
 with Tcl.Tk.Ada.Widgets.TtkTreeView; use Tcl.Tk.Ada.Widgets.TtkTreeView;
 
 package body ArchivesViews is
@@ -53,7 +56,30 @@ package body ArchivesViews is
         Create(ViewName & ".paned", "-orient horizontal");
       DirectoryTree: constant Ttk_Tree_View :=
         Create(ViewName & ".directorytree", "-show tree");
-      FilesList: constant Ttk_Tree_View := Create(ViewName & ".fileslist");
+      FilesFrame: constant Ttk_Frame := Create(ViewName & ".filesframe");
+      FilesXScroll: constant Ttk_Scrollbar :=
+        Create
+          (Widget_Image(FilesFrame) & ".scrollx",
+           "-orient horizontal -command [list " & Widget_Image(FilesFrame) &
+           ".fileslist xview]");
+      FilesYScroll: constant Ttk_Scrollbar :=
+        Create
+          (Widget_Image(FilesFrame) & ".scrolly",
+           "-orient vertical -command [list " & Widget_Image(FilesFrame) &
+           ".fileslist yview]");
+      FilesList: constant Ttk_Tree_View :=
+        Create
+          (Widget_Image(FilesFrame) & ".fileslist",
+           "-columns [list 1 2 3 4 5 6 7 8 9 10] -xscrollcommand """ &
+           Widget_Image(FilesXScroll) & " set"" -yscrollcommand """ &
+           Widget_Image(FilesYScroll) & " set""");
+      ColumnsNames: constant array(0 .. 10) of Unbounded_String :=
+        (To_Unbounded_String("Name"), To_Unbounded_String("Type"),
+         To_Unbounded_String("Modified"), To_Unbounded_String("Attributes"),
+         To_Unbounded_String("Size"), To_Unbounded_String("Packed"),
+         To_Unbounded_String("Ratio"), To_Unbounded_String("Format"),
+         To_Unbounded_String("CRC 32"), To_Unbounded_String("Name encoding"),
+         To_Unbounded_String("Result"));
       procedure Run_Command(Tcl_Command: String) is
       begin
          if Tcl_Eval(Get_Context, New_String(Tcl_Command)) /= 0 then
@@ -61,6 +87,11 @@ package body ArchivesViews is
          end if;
       end Run_Command;
    begin
+      for I in ColumnsNames'Range loop
+         Heading
+           (FilesList, "#" & Trim(Natural'Image(I), Both),
+            "-text """ & To_String(ColumnsNames(I)) & """");
+      end loop;
       Tcl.Tk.Ada.Pack.Pack(NameLabel, "-side left");
       Tcl.Tk.Ada.Pack.Pack(CloseButton, "-side right");
       Tcl.Tk.Ada.Pack.Pack(Header, "-fill x");
@@ -68,8 +99,17 @@ package body ArchivesViews is
         (Widget_Image(Paned) & " add " & Widget_Image(DirectoryTree) &
          " -weight 1");
       Run_Command
-        (Widget_Image(Paned) & " add " & Widget_Image(FilesList) &
+        (Widget_Image(Paned) & " add " & Widget_Image(FilesFrame) &
          " -weight 20");
+      Tcl.Tk.Ada.Grid.Grid(FilesList, "-column 0 -row 0 -sticky nwes");
+      Tcl.Tk.Ada.Grid.Grid(FilesYScroll, "-column 1 -row 0 -sticky nwes");
+      Tcl.Tk.Ada.Grid.Grid(FilesXScroll, "-column 0 -row 1 -sticky nwes");
+      Run_Command
+        ("grid rowconfigure " & Widget_Image(FilesFrame) & " " &
+         Widget_Image(FilesList) & " -weight 1");
+      Run_Command
+        ("grid columnconfigure " & Widget_Image(FilesFrame) & " " &
+         Widget_Image(FilesList) & " -weight 1");
       Tcl.Tk.Ada.Pack.Pack(Paned, "-fill both -expand true");
       Tcl.Tk.Ada.Pack.Pack(ArchiveView, "-fill both -expand true");
       ArchiveNumber := ArchiveNumber + 1;

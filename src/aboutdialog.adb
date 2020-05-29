541a63e52ae674bedc3eb0e0196d0fc80ffd56e6
Bartek thindil Jasicki <thindil@laeran.pl>
2020-05-29 10:20:53 +0200

    better detection of height for about dialog

--- src/aboutdialog.adb
+++ src/aboutdialog.adb
@@ -64,6 +64,7 @@ package body AboutDialog is
         Create
           (".aboutdialog.librariesframe",
            "-text {AZip is made with the following free, open source components}");
+      Width, ReqestedWidth, Height: Natural := 0;
       procedure AddLinkButton
         (Name, URL: String; Row: Natural; Column: Natural := 1;
          Text: String := "") is
@@ -81,72 +82,74 @@ package body AboutDialog is
               "} -style Toolbutton -command {OpenLink " & " " & URL & "}");
          Tcl.Tk.Ada.Grid.Grid
            (Button,
-            "-column" & Natural'Image(Column) & " -row" & Natural'Image(Row));
+            "-column" & Natural'Image(Column) & " -row" & Natural'Image(Row) &
+            " -sticky w");
       end AddLinkButton;
+      procedure AddLabel(Name, Text, GridOptions: String) is
+         Label: constant Ttk_Label := Create(Name, "-text {" & Text & "}");
+      begin
+         Tcl.Tk.Ada.Grid.Grid(Label, GridOptions);
+         ReqestedWidth := Natural'Value(Winfo_Get(Label, "reqwidth"));
+         Height := Natural'Value(Winfo_Get(Label, "reqheight"));
+         if ReqestedWidth > Width then
+            Width := ReqestedWidth;
+         end if;
+      end AddLabel;
    begin
       if Tcl.Tk.Ada.Busy.Status(MainWindow) = "0" then
          Tcl.Tk.Ada.Busy.Busy(MainWindow);
       end if;
       Label := Create(".aboutdialog.logo", "-image logo");
       Tcl.Tk.Ada.Grid.Grid(Label);
-      Label :=
-        Create
-          (".aboutdialog.infoframe.general",
-           "-text {AZip - A portable Zip Archive Manager}");
-      Tcl.Tk.Ada.Grid.Grid(Label, "-columnspan 2");
-      Label :=
-        Create
-          (".aboutdialog.infoframe.copyright",
-           "-text {Copyright © Gautier de Montmollin 2012 .. 2019}");
-      Tcl.Tk.Ada.Grid.Grid(Label, "-row 1 -columnspan 2");
-      Label :=
-        Create
-          (".aboutdialog.infoframe.license",
-           "-text {MIT Open Source License}");
-      Tcl.Tk.Ada.Grid.Grid(Label, "-row 2 -columnspan 2");
-      Label := Create(".aboutdialog.infoframe.website", "-text {Internet:}");
-      Tcl.Tk.Ada.Grid.Grid(Label, "-row 3");
+      AddLabel
+        (".aboutdialog.infoframe.general",
+         "AZip - A portable Zip Archive Manager", "-columnspan 2 -sticky w");
+      AddLabel
+        (".aboutdialog.infoframe.copyright",
+         "Copyright © Gautier de Montmollin 2012 .. 2019",
+         "-row 1 -columnspan 2");
+      AddLabel
+        (".aboutdialog.infoframe.license", "MIT Open Source License",
+         "-row 2 -columnspan 2 -sticky w");
+      AddLabel
+        (".aboutdialog.infoframe.websize", "Internet: ", "-row 3 -sticky w");
       AddLinkButton
         (".aboutdialog.infoframe.websitebutton", "http://azip.sf.net", 3);
-      Label := Create(".aboutdialog.infoframe.version", "-text {Version:}");
-      Tcl.Tk.Ada.Grid.Grid(Label, "-row 4");
-      Label := Create(".aboutdialog.infoframe.versionnumber", "-text {2.36}");
-      Tcl.Tk.Ada.Grid.Grid(Label, "-column 1 -row 4");
+      AddLabel
+        (".aboutdialog.infoframe.version", "Version:", "-row 4 -sticky w");
+      AddLabel
+        (".aboutdialog.infoframe.versionnumber", "2.36", "-column 1 -row 4");
       Tcl.Tk.Ada.Grid.Grid(InfoFrame, "-column 1 -row 0");
+      Tcl.Tk.Ada.Grid.Column_Configure(AboutDialog, InfoFrame, "-weight 1");
       AddLinkButton
         (".aboutdialog.librariesframe.gnatlink",
          "https://www.adacore.com/community", 0, 0,
          "GNAT - free Ada compiler");
-      Label :=
-        Create
-          (".aboutdialog.librariesframe.gnatversion",
-           "-text {version GPL 2017 (20170515-63)}");
-      Tcl.Tk.Ada.Grid.Grid(Label, "-column 1 -row 0");
+      AddLabel
+        (".aboutdialog.librariesframe.gnatversion",
+         "version GPL 2017 (20170515-63)", "-column 1 -row 0");
       AddLinkButton
         (".aboutdialog.librariesframe.gnavi",
          "https://sourceforge.net/projects/gnavi/", 1, 0, "GNAVI / GWindows");
       AddLinkButton
         (".aboutdialog.librariesframe.reseditlink", "http://www.resedit.net/",
          2, 0, "ResEdit");
-      Label :=
-        Create
-          (".aboutdialog.librariesframe.reseditinfo",
-           "-text {(freeware, not open-source)}");
-      Tcl.Tk.Ada.Grid.Grid(Label, "-column 1 -row 2");
+      AddLabel
+        (".aboutdialog.librariesframe.reseditinfo",
+         "(freeware, not open-source)", "-column 1 -row 2 -sticky w");
       AddLinkButton
         (".aboutdialog.librariesframe.zipadalink",
          "https://unzip-ada.sourceforge.io/", 3, 0, "Zip-Ada");
-      Label :=
-        Create
-          (".aboutdialog.librariesframe.zipadaversion",
-           "-text {version 55, ref 22-Now-2018}");
-      Tcl.Tk.Ada.Grid.Grid(Label, "-column 1 -row 3");
+      AddLabel
+        (".aboutdialog.librariesframe.zipadaversion",
+         "version 55, ref 22-Now-2018", "-column 1 -row 3 -sticky w");
       AddLinkButton
         (".aboutdialog.librariesframe.inifilelink",
          "https://sourceforge.net/projects/ini-files/", 4, 0,
          "Ini file manager");
       Tcl.Tk.Ada.Grid.Grid
         (LibrariesFrame, "-column 0 -row 1 -columnspan 2 -sticky we");
+      Height := Height * 14;
       Button :=
         Create
           (".aboutdialog.buttonbox.credits",
@@ -159,7 +162,8 @@ package body AboutDialog is
       Tcl.Tk.Ada.Grid.Grid(Button, "-column 1 -row 0");
       Tcl.Tk.Ada.Grid.Grid
         (ButtonBox, "-column 1 -row 2 -sticky we -columnspan 2");
-      SetDialog(AboutDialog, "About AZip", 500, 400);
+      Height := Height + Natural'Value(Winfo_Get(Button, "reqheight"));
+      SetDialog(AboutDialog, "About AZip", Width, Height);
       return TCL_OK;
    end Show_About_Command;
 
